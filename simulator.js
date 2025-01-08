c934e29558093f5704b9d323b4000f138fc2b592
NogFar <52389872+NogFar@users.noreply.github.com>
2025-01-08 20:17:03 -0300

    refactor: ALU, IR, testSimulation
    ALU agora executa com os valores em seu registrador; IR exibe o código da macroinstrução sendo executada; Execução de código foi feita com um array de instruções

--- simulator.js
+++ simulator.js
@@ -34,22 +34,22 @@ class Mic1{
         this.memory = new Array(256).fill(0); 
     }
 
-    ALU(op, input1, input2) {
+    ALU(op) {
         switch (op) {
-            case 'ADD': return input1 + input2;
-            case 'SUB': return input1 - input2;
-            case 'AND': return input1 & input2;
-            case 'OR': return input1 | input2;
+            case 'ADD': return this.registers.ALU[0] + this.registers.ALU[1];
+            case 'SUB': return this.registers.ALU[0] - this.registers.ALU[1];
+            case 'AND': return this.registers.ALU[0] & this.registers.ALU[1];
+            case 'OR': return this.registers.ALU[0] | this.registers.ALU[1];
             default: return 0;
         }
     }
-    
+
     // Unidade de Controle - Função para executar uma microinstrução
     execMicroInst(microOp) {
         switch (microOp) {
-            case 'MAR <- PC':
-                this.registers.MAR = this.registers.PC;
-                break;
+            // case 'MAR <- PC':
+            //     this.registers.MAR = this.registers.PC;
+            //     break;
             case 'MBR <- M[MAR]':
                 this.registers.MBR = this.memory[this.registers.MAR];
                 break;
@@ -63,7 +63,8 @@ class Mic1{
                 this.registers.A = this.registers.AC;
                 break;
             case 'AC <- A + MBR':
-                this.registers.AC = this.ALU('ADD', this.registers.A, this.registers.MBR);
+                this.registers.ALU = [this.registers.A, this.registers.MBR]
+                this.registers.AC = this.ALU('ADD');
                 break;
             case 'MBR <- AC':
                 this.registers.MBR = this.registers.AC;
@@ -88,118 +89,142 @@ class Mic1{
 
     // Simulador de Instruções de Alto Nível
     execMacroInst(instruction, address) {
+        let addressBit = address.toString(2).padStart(12, '0');
         switch (instruction) {
             case 'LODD':
-                this.execMicroInst('MAR <- PC');      // Etapa 1: Carrega o endereço no MAR
+                this.registers.IR = "0000" + addressBit;
+                this.registers.MAR = address;     // Etapa 1: Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Etapa 2: Lê o valor da memória para MBR
                 this.execMicroInst('AC <- MBR');      // Etapa 3: Transfere para o acumulador AC
                 break;
             case 'STOD':
-                this.execMicroInst('MAR <- PC');      // Etapa 1: Carrega o endereço no MAR
+                this.registers.IR = "0001" + addressBit;
+                this.registers.MAR = address;     // Etapa 1: Carrega o endereço no MAR
                 this.execMicroInst('MBR <- AC');      // Etapa 2: Carrega o valor do acumulador para MBR
                 this.execMicroInst('M[MAR] <- MBR');     // Etapa 3: Armazena na memória
                 break;
             case 'ADDD':
-                this.execMicroInst('MAR <- PC');      // Etapa 1: Carrega o endereço no MAR
+                this.registers.IR = "0010" + addressBit;
+                this.registers.MAR = address;     // Etapa 1: Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Etapa 2: Lê o valor da memória para MBR
                 this.execMicroInst('A <- AC');        // Etapa 3: Armazena o AC em A
                 this.execMicroInst('AC <- A + MBR');  // Etapa 4: Soma A e MBR e armazena em AC
                 break;
             case 'SUBD':
-                this.execMicroInst('MAR <- PC');      // Etapa 1: Carrega o endereço no MAR
+                this.registers.IR = "0011" + addressBit;
+                this.registers.MAR = address;     // Etapa 1: Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Etapa 2: Lê o valor da memória para MBR
                 this.execMicroInst('AC <- AC - MBR'); // Etapa 3: Subtrai o valor de MBR de AC
                 break;
-            case 'JPOS': // Jump Positive
-                this.execMicroInst('MAR <- PC');  // Carrega o endereço no MAR
+            case 'JPOS': // Jump 
+                this.registers.IR = "0100" + addressBit;
+                this.registers.MAR = address; // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória
                 this.execMicroInst('PC <- (AC > 0) ? MBR : PC');  // Se AC > 0, pula para o valor de MBR
                 break;
             case 'JZER': // Jump Zero
-                this.execMicroInst('MAR <- PC');  // Carrega o endereço no MAR
+                this.registers.IR = "0101" + addressBit;
+                this.registers.MAR = address; // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória
                 this.execMicroInst('PC <- (AC === 0) ? MBR : PC');  // Se AC == 0, pula para o valor de MBR
                 break;
             case 'JUMP': // Jump
-                this.execMicroInst('MAR <- PC');  // Carrega o endereço no MAR
+                this.registers.IR = "0110" + addressBit;
+                this.registers.MAR = address; // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória
                 this.execMicroInst('PC <- MBR');  // Pula para o valor de MBR
                 break;
             case 'LOCO': // Load Constant
-                this.execMicroInst('MAR <- PC');  // Carrega o endereço no MAR
+                this.registers.IR = "0111" + addressBit;
+                this.registers.MAR = address; // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória
                 this.execMicroInst('AC <- MBR');  // Carrega o valor constante no AC
                 break;
             case 'LODL': // Load Local
-                this.execMicroInst('MAR <- PC');      // Carrega o endereço no MAR
+                this.registers.IR = "1000" + addressBit;
+                this.registers.MAR = address;     // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória para MBR
                 this.execMicroInst('AC <- MBR');      // Carrega o valor no acumulador
                 break;
             case 'STOL': // Store Local
-                this.execMicroInst('MAR <- PC');      // Carrega o endereço no MAR
+                this.registers.IR = "1001" + addressBit;
+                this.registers.MAR = address;     // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- AC');      // Transfere o valor do AC para o MBR
                 this.execMicroInst('M[MAR] <- MBR');  // Armazena o valor no endereço
                 break;
             case 'ADDL': // Add Local
-                this.execMicroInst('MAR <- PC');      // Carrega o endereço no MAR
+                this.registers.IR = "1010" + addressBit;
+                this.registers.MAR = address;     // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória para MBR
                 this.execMicroInst('AC <- AC + MBR'); // Soma o valor de AC com o valor de MBR
                 break;
             case 'SUBL': // Subtract Local
-                this.execMicroInst('MAR <- PC');      // Carrega o endereço no MAR
+                this.registers.IR = "1011" + addressBit;
+                this.registers.MAR = address;     // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória para MBR
                 this.execMicroInst('AC <- AC - MBR'); // Subtrai o valor de MBR de AC
                 break;
             case 'JNEG': // Jump Negative
-                this.execMicroInst('MAR <- PC');      // Carrega o endereço no MAR
+                this.registers.IR = "1100" + addressBit;
+                this.registers.MAR = address;     // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória para MBR
                 this.execMicroInst('PC <- (AC < 0) ? MBR : PC'); // Se AC < 0, pula para o valor de MBR
                 break;
             case 'JNZE': // Jump Not Zero
-                this.execMicroInst('MAR <- PC');      // Carrega o endereço no MAR
+                this.registers.IR = "1101" + addressBit;
+                this.registers.MAR = address;     // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória para MBR
                 this.execMicroInst('PC <- (AC !== 0) ? MBR : PC'); // Se AC != 0, pula para o valor de MBR
                 break;
             case 'CALL': // Call Subroutine
-                this.execMicroInst('MAR <- PC');  // Carrega o endereço no MAR
+                this.registers.IR = "1110" + addressBit;
+                this.registers.MAR = address; // Carrega o endereço no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor da memória
                 this.execMicroInst('SP <- SP - 1'); // Decrementa o Stack Pointer (SP)
                 this.execMicroInst('M[SP] <- PC');  // Armazena o endereço de retorno no stack
                 this.execMicroInst('PC <- MBR');  // Pula para a sub-rotina
                 break;   
             case 'PSHI': // Push Indirect
+                this.registers.IR = "1111".padEnd(16, '0');
                 this.execMicroInst('MAR <- SP');  // Carrega o valor do stack pointer no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor da memória para o MBR
                 this.execMicroInst('M[MAR] <- MBR');  // Armazena o valor no stack
                 break;
             case 'POPI': // Pop Indirect
+                this.registers.IR = "1111001".padEnd(16, '0');
                 this.execMicroInst('MAR <- SP');  // Carrega o valor do stack pointer no MAR
                 this.execMicroInst('MBR <- M[MAR]');  // Lê o valor de memória para MBR
                 this.execMicroInst('SP <- SP + 1');  // Incrementa o Stack Pointer
                 break;
             case 'PUSH': // Push
+                this.registers.IR = "111101".padEnd(16, '0');
                 this.execMicroInst('SP <- SP - 1');   // Decrementa o Stack Pointer
                 this.execMicroInst('M[SP] <- AC');   // Armazena o valor de AC no topo da pilha
                 break;
             case 'POP': // Pop
+                this.registers.IR = "1111011".padEnd(16, '0');
                 this.execMicroInst('MBR <- M[SP]');  // Lê o valor do topo da pilha para MBR
                 this.execMicroInst('AC <- MBR');     // Transfere o valor de MBR para o AC
                 this.execMicroInst('SP <- SP + 1');   // Incrementa o Stack Pointer
                 break;
             case 'RETN': // Return from Subroutine
+                this.registers.IR = "11111".padEnd(16, '0');
                 this.execMicroInst('MBR <- M[SP]');   // Lê o valor de retorno da pilha para MBR
                 this.execMicroInst('PC <- MBR');      // Transfere o valor de MBR para o PC
                 this.execMicroInst('SP <- SP + 1');   // Incrementa o Stack Pointer
                 break;
             case 'SWAP': // Swap
+                this.registers.IR = "1111101".padEnd(16, '0');
                 this.execMicroInst('MBR <- AC');      // Transfere o valor de AC para MBR
                 this.execMicroInst('AC <- M[SP]');    // Carrega o valor de memória no topo da pilha para AC
                 this.execMicroInst('M[SP] <- MBR');   // Armazena o valor de MBR de volta na pilha
                 break;
             case 'INSP': // Increment Stack Pointer
+                this.registers.IR = "11111100" + address.toString(2).padStart(8, '0');
                 this.execMicroInst('SP <- SP + 1');   // Incrementa o Stack Pointer
                 break;
             case 'DESP': // Decrement Stack Pointer
+                this.registers.IR = "11111110" + address.toString(2).padStart(8, '0');
                 this.execMicroInst('SP <- SP - 1');   // Decrementa o Stack Pointer
                 break;
             default:
@@ -212,17 +237,27 @@ class Mic1{
     // Teste de Simulação
     testSimulation() {
         this.memory[0] = 10;  // Endereço de teste para LOAD e ADD
-        this.memory[1] = 20;  // Outro endereço de teste para operações de memória
+        this.memory[1] = 20;  // Outro endereço de teste para operações de 
+        let instructions = [
+            ['LODD', 0],
+            ['ADDD', 1],
+            ['STOD', 6],
+        ]
 
         this.registers.PC = 0;  // Definir contador de programa para o endereço 0
-        this.execMacroInst('LODD', 0);  // Carregar valor do endereço 0 para AC
-        console.log("Valor de AC após LODD:", this.registers.AC);  // Deve exibir 10
+        
+        instructions.forEach((inst)=> {
+            this.execMacroInst(inst[0], inst[1]);
+        })
 
-        this.execMacroInst('ADDD', 1);  // Soma o valor do endereço 1 ao AC
-        console.log("Valor de AC após ADDD:", this.registers.AC);  // Deve exibir 30
+        // this.execMacroInst('LODD', 0);  // Carregar valor do endereço 0 para AC
+        // console.log("Valor de AC após LODD:", this.registers.AC);  // Deve exibir 10
 
-        this.execMacroInst('STOD', 2);  // Armazena o valor de AC no endereço 2
-        console.log("Valor de Memory[2] após STOD:", this.memory[2]);  // Deve exibir 30
+        // this.execMacroInst('ADDD', 1);  // Soma o valor do endereço 1 ao AC
+        // console.log("Valor de AC após ADDD:", this.registers.AC);  // Deve exibir 30
+
+        // this.execMacroInst('STOD', 6);  // Armazena o valor de AC no endereço 2
+        // console.log("Valor de Memory[2] após STOD:", this.memory[2]);  // Deve exibir 30
     }
 }
 
